f0162d5dd587ffb93e25612012ab3b1eaf378b31
Guoliang Cao <gcao99@gmail.com>
2025-12-23 23:01:53 -0500

    Fix repl-on-error flag

--- src/commands/run.nim
+++ src/commands/run.nim
@@ -95,9 +95,7 @@ proc handle*(cmd: string, args: seq[string]): CommandResult =
   let options = parse_options(args)
   setup_logger(options.debugging)
   proc handle_exec_error(e: ref CatchableError): CommandResult =
-    if options.repl_on_error:
-      return failure(e.msg)
-    if VM.current_exception != NIL and VM.frame != nil:
+    if options.repl_on_error and VM.current_exception != NIL and VM.frame != nil:
       stderr.writeLine("Error: " & e.msg)
       discard run_repl_on_error(VM, VM.current_exception)
       return failure("")
