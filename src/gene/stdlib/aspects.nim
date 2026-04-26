7ee71e958794344c57f225a05709686b57edfa3b
Guoliang Cao <gcao99@gmail.com>
2026-04-26 13:28:37 -0400

    feat: Added the interceptor definition macro and shared class applicati…
    - src/gene/stdlib/aspects.nim
    - src/gene/compiler/operators.nim
    
    GSD-Task: S01/T01

--- src/gene/stdlib/aspects.nim
+++ src/gene/stdlib/aspects.nim
@@ -71,27 +71,27 @@ proc resolve_advice_callable(callable_val: Value, caller_frame: Frame): Value =
   else:
     not_allowed("advice callable must be a symbol")
 
-proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame): Value {.gcsafe.} =
+proc parse_aspect_macro(form_label: string, vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame): Value {.gcsafe.} =
   {.cast(gcsafe).}:
     let gene = gene_value.gene
     if gene.children.len < 2:
-      not_allowed("aspect requires a name and method parameters")
+      not_allowed(form_label & " requires a name and method parameters")
 
     let name_val = gene.children[0]
     if name_val.kind != VkSymbol:
-      not_allowed("aspect name must be a symbol")
+      not_allowed(form_label & " name must be a symbol")
     let name = name_val.str
 
     let params_val = gene.children[1]
     if params_val.kind != VkArray:
-      not_allowed("aspect parameter list must be an array")
+      not_allowed(form_label & " parameter list must be an array")
 
     var param_names: seq[string] = @[]
     for p in array_data(params_val):
       if p.kind == VkSymbol:
         param_names.add(p.str)
       else:
-        not_allowed("aspect parameter must be a symbol")
+        not_allowed(form_label & " parameter must be a symbol")
 
     let aspect = Aspect(
       name: name,
@@ -107,15 +107,15 @@ proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame
     for i in 2..<gene.children.len:
       let advice_def = gene.children[i]
       if advice_def.kind != VkGene:
-        not_allowed("advice definition must be a gene expression")
+        not_allowed(form_label & " advice definition must be a gene expression")
 
       let advice_gene = advice_def.gene
       if advice_gene.children.len < 2:
-        not_allowed("advice requires type and target")
+        not_allowed(form_label & " advice requires type and target")
 
       let advice_type = advice_gene.type
       if advice_type.kind != VkSymbol:
-        not_allowed("advice type must be a symbol")
+        not_allowed(form_label & " advice type must be a symbol")
       let advice_type_str = advice_type.str
 
       var replace_result = false
@@ -124,15 +124,15 @@ proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame
         let replace_val = advice_gene.props[replace_key]
         replace_result = (replace_val == NIL or replace_val == PLACEHOLDER) or replace_val.to_bool()
         if replace_result and advice_type_str != "after":
-          not_allowed("replace_result is only allowed for after advices")
+          not_allowed("replace_result is only allowed for after " & form_label & " advices")
 
       let target = advice_gene.children[0]
       if target.kind != VkSymbol:
-        not_allowed("advice target must be a method parameter symbol")
+        not_allowed(form_label & " advice target must be a method parameter symbol")
       let target_name = target.str
 
       if not (target_name in param_names):
-        not_allowed("advice target '" & target_name & "' is not a defined method parameter")
+        not_allowed(form_label & " advice target '" & target_name & "' is not a defined method parameter")
 
       var advice_val: Value
       var user_arg_count = -1
@@ -198,7 +198,7 @@ proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame
           aspect.before_filter_advices[target_name] = @[]
         aspect.before_filter_advices[target_name].add(advice_val)
       else:
-        not_allowed("unknown advice type: " & advice_type_str)
+        not_allowed("unknown " & form_label & " advice type: " & advice_type_str)
 
     let aspect_ref = new_ref(VkAspect)
     aspect_ref.aspect = aspect
@@ -207,6 +207,12 @@ proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame
     caller_frame.ns[name.to_key()] = aspect_val
 
     return aspect_val
+
+proc aspect_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame): Value {.gcsafe.} =
+  parse_aspect_macro("aspect", vm, gene_value, caller_frame)
+
+proc interceptor_macro(vm: ptr VirtualMachine, gene_value: Value, caller_frame: Frame): Value {.gcsafe.} =
+  parse_aspect_macro("interceptor", vm, gene_value, caller_frame)
 
 proc create_interception_value(original: Value, aspect_value: Value, param_name: string): Value =
   let interception = Interception(
@@ -219,47 +225,68 @@ proc create_interception_value(original: Value, aspect_value: Value, param_name:
   interception_ref.interception = interception
   interception_ref.to_ref_value()
 
-proc aspect_apply(vm: ptr VirtualMachine, args: ptr UncheckedArray[Value], arg_count: int, has_keyword_args: bool): Value {.gcsafe.} =
-  if arg_count < 2:
-    not_allowed("aspect.apply requires self and class arguments")
-
-  let self = get_positional_arg(args, 0, has_keyword_args)
+proc apply_aspect_to_class(label: string, self: Value, class_arg: Value, method_name_vals: seq[Value]): Value =
   if self.kind != VkAspect:
-    not_allowed("apply must be called on an aspect")
+    not_allowed(label & " must be called on an aspect")
 
   let aspect = self.ref.aspect
 
-  let class_arg = get_positional_arg(args, 1, has_keyword_args)
   if class_arg.kind != VkClass:
-    not_allowed("aspect.apply requires a class argument")
+    not_allowed(label & " requires a class argument")
 
   let class = class_arg.ref.class
 
-  let positional = get_positional_count(arg_count, has_keyword_args)
-  if positional - 2 != aspect.param_names.len:
-    not_allowed("aspect.apply requires " & $aspect.param_names.len & " method name arguments")
+  if method_name_vals.len != aspect.param_names.len:
+    not_allowed(label & " requires " & $aspect.param_names.len & " method name arguments")
 
   let applied = new_array_value()
   for i in 0..<aspect.param_names.len:
     let param_name = aspect.param_names[i]
-    let method_name_val = get_positional_arg(args, i + 2, has_keyword_args)
+    let method_name_val = method_name_vals[i]
     var method_name = ""
     case method_name_val.kind
     of VkString, VkSymbol:
       method_name = method_name_val.str
     else:
-      not_allowed("method name must be a string or symbol")
+      not_allowed(label & " method name must be a string or symbol")
 
     let method_key = method_name.to_key()
     if not class.methods.hasKey(method_key):
-      not_allowed("class does not have method: " & method_name)
+      not_allowed(label & " class does not have method: " & method_name)
 
     let original_method = class.methods[method_key]
     let interception_val = create_interception_value(original_method.callable, self, param_name)
     class.methods[method_key].callable = interception_val
+    class.version.inc()
+    if class.runtime_type != nil:
+      class.runtime_type.methods[method_key] = interception_val
     array_data(applied).add(interception_val)
 
   return applied
+
+proc collect_method_name_args(label: string, args: ptr UncheckedArray[Value], arg_count: int,
+                              has_keyword_args: bool): seq[Value] =
+  if has_keyword_args:
+    not_allowed(label & " does not accept keyword arguments")
+
+  if arg_count < 2:
+    not_allowed(label & " requires self and class arguments")
+
+  let positional = get_positional_count(arg_count, has_keyword_args)
+  for i in 2..<positional:
+    result.add(get_positional_arg(args, i, has_keyword_args))
+
+proc aspect_apply(vm: ptr VirtualMachine, args: ptr UncheckedArray[Value], arg_count: int, has_keyword_args: bool): Value {.gcsafe.} =
+  let method_name_vals = collect_method_name_args("aspect.apply", args, arg_count, has_keyword_args)
+  let self = get_positional_arg(args, 0, has_keyword_args)
+  let class_arg = get_positional_arg(args, 1, has_keyword_args)
+  apply_aspect_to_class("aspect.apply", self, class_arg, method_name_vals)
+
+proc aspect_call(vm: ptr VirtualMachine, args: ptr UncheckedArray[Value], arg_count: int, has_keyword_args: bool): Value {.gcsafe.} =
+  let method_name_vals = collect_method_name_args("interceptor application", args, arg_count, has_keyword_args)
+  let self = get_positional_arg(args, 0, has_keyword_args)
+  let class_arg = get_positional_arg(args, 1, has_keyword_args)
+  apply_aspect_to_class("interceptor application", self, class_arg, method_name_vals)
 
 proc aspect_apply_fn(vm: ptr VirtualMachine, args: ptr UncheckedArray[Value], arg_count: int, has_keyword_args: bool): Value {.gcsafe.} =
   if arg_count < 3:
@@ -320,8 +347,13 @@ proc init_aspect_support*() =
   aspect_macro_ref.native_macro = aspect_macro
   global_ns["aspect".to_key()] = aspect_macro_ref.to_ref_value()
 
+  var interceptor_macro_ref = new_ref(VkNativeMacro)
+  interceptor_macro_ref.native_macro = interceptor_macro
+  global_ns["interceptor".to_key()] = interceptor_macro_ref.to_ref_value()
+
   let aspect_class = new_class("Aspect")
   aspect_class.def_native_method("apply", aspect_apply)
+  aspect_class.def_native_method("call", aspect_call)
   aspect_class.def_native_method("apply-fn", aspect_apply_fn)
   aspect_class.def_native_method("enable-interception", aspect_enable_interception)
   aspect_class.def_native_method("disable-interception", aspect_disable_interception)
@@ -329,4 +361,6 @@ proc init_aspect_support*() =
   aspect_class_ref.class = aspect_class
   App.app.aspect_class = aspect_class_ref.to_ref_value()
   App.app.gene_ns.ns["Aspect".to_key()] = App.app.aspect_class
+  App.app.gene_ns.ns["Interceptor".to_key()] = App.app.aspect_class
   global_ns["Aspect".to_key()] = App.app.aspect_class
+  global_ns["Interceptor".to_key()] = App.app.aspect_class
